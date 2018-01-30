e138cc5711f66fa083ab5fdc1804e6a61261dcb9
Grawul <tobias.bloch@freenet.de>
2018-01-30 11:41:13 +0100

    initial commit

--- main-application/src/main/java/section/Wing.java
+++ main-application/src/main/java/section/Wing.java
@@ -1,6 +1,37 @@
 package section;
 
 import event.Subscriber;
+import logging.LogEngine;
+
+import java.util.ArrayList;
 
 public class Wing extends Subscriber {
+    private int wingIndex;
+
+    private ArrayList<Object> weatherRadarPorts;
+
+    public Wing(int wingIndex) {
+        this.wingIndex = wingIndex;
+        weatherRadarPorts = new ArrayList<>();
+        build();
+        printStatus();
+    }
+
+    public void build() {
+        // please add here
+    }
+
+    public void printStatus() {
+        try {
+            LogEngine.instance.write(wingIndex == 0 ? "--- left Wing ---" : "--- right Wing ---");
+
+            // please add here
+
+            LogEngine.instance.write("");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // please add here
 }
