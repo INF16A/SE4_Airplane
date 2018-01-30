649eafc1c5466ad9242cc351d26d3107bd696cd8
Paddy3108 <Patrick.Eichert@web.de>
2018-01-30 17:14:54 +0100

    wrote FactoryTests

---
+++ main-application/src/test/java/HydraulicPumpTest.java
@@ -0,0 +1,18 @@
+import factory.EngineFactory;
+import factory.HydraulicPumpFactory;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class HydraulicPumpTest {
+
+    private Object componentPort;
+
+
+    @Test
+    public void build() {
+        componentPort = HydraulicPumpFactory.build();
+        assertNotNull(componentPort);
+    }
+
+}
