4b068853845edc39843f47b3c8ed837c90b5524b
Carsten Hagemann <carsten@chagemann.de>
2018-01-30 12:19:11 +0100

    implement Management CopstOptimizer

--- components/14_management_costoptimizer/src/CostOptimizer.java
+++ components/14_management_costoptimizer/src/CostOptimizer.java
@@ -3,7 +3,10 @@ import java.util.ArrayList;
 /**
  * Created by chag0709 on 30.01.18 - Kaufland Informationssysteme 2018.
  */
-public class CostOptimizer implements ICostOptimizer {
+public class CostOptimizer {
+
+    public Port port;
+    private static CostOptimizer instance = new CostOptimizer();
 
     private String manufacturer;
     private String type;
@@ -11,40 +14,50 @@ public class CostOptimizer implements ICostOptimizer {
     private boolean isOn;
     private ArrayList<CheckPoint> checkPointList;
 
-    @Override
-    public String version() {
-        return null;
+    private static final int COST_INDEX_HARDCODE = 2;
+
+    private CostOptimizer() {
+        this.port = new Port();
     }
 
-    @Override
-    public boolean on() {
-        return false;
+    public static CostOptimizer getInstance() {
+        return instance;
     }
 
-    @Override
-    public boolean off() {
-        return false;
+    @SuppressWarnings("ConstantConditions") // It may not be possible to start or turn off the system
+    private class Port implements ICostOptimizer {
+
+        public String version() {
+            return "CostOptimizer 1.0.0";
+        }
+
+        public boolean on() {
+            isOn = true;
+            return isOn;
+        }
+
+        public boolean off() {
+            isOn = false;
+            return isOn;
+        }
+
+        public int add(CheckPoint checkPoint) {
+            checkPointList.add(checkPoint);
+            return checkPointList.size();
+        }
+
+        public int remove(int checkPoint) {
+            checkPointList.remove(checkPoint);
+            return checkPointList.size();
+        }
+
+        public int optimize(ArrayList<CheckPoint> checkPointList) {
+            // do voodoo aka https://de.wikipedia.org/wiki/Cost_Index
+            return COST_INDEX_HARDCODE;
+        }
+
+        public boolean validate(int costIndex) {
+            return costIndex >= 1 || costIndex <= 3;
+        }
     }
-
-    @Override
-    public int add(CheckPoint checkPoint) {
-        return 0;
-    }
-
-    @Override
-    public int remove(int checkPoint) {
-        return 0;
-    }
-
-    @Override
-    public int optimize(ArrayList<CheckPoint> checkPointList) {
-        return 0;
-    }
-
-    @Override
-    public boolean validate(int costIndex) {
-        return false;
-    }
-
-
 }
