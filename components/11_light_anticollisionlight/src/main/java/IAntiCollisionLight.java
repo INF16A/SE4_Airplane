260d83709fa4f50ff36c338c53536c2af6dfee95
PiranhaGod420 <f.hanschristian@gmail.com>
2018-01-30 14:18:36 +0100

    Updated Structure

---
+++ components/11_light_anticollisionlight/src/main/java/IAntiCollisionLight.java
@@ -0,0 +1,8 @@
+/**
+ * Created by SolDeEcuador on 30/01/2018.
+ */
+public interface IAntiCollisionLight {
+    String version();
+    boolean on();
+    boolean off();
+}
---
+++ components/11_light_landinglight/src/main/java/LandingLight.java
@@ -0,0 +1,9 @@
+/**
+ * Created by SolDeEcuador on 30/01/2018.
+ */
+public class LandingLight {
+    private String manufacturer;
+    private String type;
+    private String id;
+    private boolean isOn;
+}
