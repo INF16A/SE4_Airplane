02244a40b5b6445a104c902900e4298cef49bffe
armbeck16 <arm.beck.16@lehre.mosbach.dhbw.de>
2018-01-30 13:53:46 +0100

    exhaust detector sensor portified

--- components/07_ExhaustGasTemperatureSensor/src/main/java/ExhaustGasTemperatureSensor.java
+++ components/07_ExhaustGasTemperatureSensor/src/main/java/ExhaustGasTemperatureSensor.java
@@ -1,4 +1,8 @@
-public class ExhaustGasTemperatureSensor implements IExhaustGasTemperatureSensor {
+public class ExhaustGasTemperatureSensor {
+
+    private ExhaustGasTemperatureSensor() {
+
+    }
 
     public String version() {
         return "Super fancy exhaust sensor 99.NaN.1e-10";
@@ -9,13 +13,38 @@ public class ExhaustGasTemperatureSensor implements IExhaustGasTemperatureSensor
     }
 
     public boolean alarmMajor(int threshold) {
-        return threshold==100;
+        return threshold == 100;
     }
 
     public boolean alarmCritical(int threshold) {
-        return threshold==200;
+        return threshold == 200;
     }
-    private  String manufacturer="DHBW";
-    private  String type="zero";
-    private  String id="0";
+
+    private String manufacturer = "DHBW";
+    private String type = "zero";
+    private String id = "0";
+    public Port port = new Port();
+    private static ExhaustGasTemperatureSensor instance = new ExhaustGasTemperatureSensor();
+
+    public class Port implements IExhaustGasTemperatureSensor {
+        @Override
+        public boolean alarmCritical(int threshold) {
+            return instance.alarmCritical(threshold);
+        }
+
+        @Override
+        public boolean alarmMajor(int threshold) {
+            return instance.alarmMajor(threshold);
+        }
+
+        @Override
+        public int measure() {
+            return instance.measure();
+        }
+
+        @Override
+        public String version() {
+            return instance.version();
+        }
+    }
 }
