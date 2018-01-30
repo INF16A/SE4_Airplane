ef50a223a0ee2f9c5b5348e27e1deffde6e4ea24
JanJan <janmalch@gmail.com>
2018-01-30 16:43:52 +0100

    Added Events for LeftAileron, RightAileron, Rudder, Spoiler

---
+++ main-application/src/main/java/event/rightaileron/RightAileronFullDown.java
@@ -0,0 +1,19 @@
+package event.rightaileron;
+
+public class RightAileronFullDown {
+
+    private int id;
+
+    public RightAileronFullDown(int id) {
+        this.id = id;
+    }
+
+    public String toString() {
+
+        return "{ RightAileronFullDown : id = " + id + " }";
+
+    }
+    public int getId() {
+        return id;
+    }
+}
