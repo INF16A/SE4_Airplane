1b6169f8dece708a75a76c82d6009549ae6e9819
alexkrieg <alexkrieg93@googlemail.com>
2018-01-30 16:44:57 +0100

    POM Update

--- components/05_FuelTank/src/test/java/FuelTankTest.java
+++ components/05_FuelTank/src/test/java/FuelTankTest.java
@@ -1,26 +1,26 @@
 package test.java;
 
 import main.java.FuelTank;
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class FuelTankTest {
 	private FuelTank.Port port;
 
-	@BeforeEach
+	@Before
 	public void init(){
 		port = FuelTank.getInstance().port;
 	}
 
 	@Test
 	public void version(){
-		assertEquals("FuelTank - Version 1.0", port.version());
+		Assert.assertEquals("FuelTank - Version 1.0", port.version());
 	}
 
 	@Test
 	public void refill() {
-		assertEquals(250, port.refill());
+		Assert.assertEquals(250, port.refill());
 	}
 
 	@Test
@@ -28,13 +28,13 @@ public class FuelTankTest {
 		port.takeOut(100);
 		int cur = port.refill(0);
 		int after = port.refill(400);
-		assertTrue(after > cur);
+		Assert.assertTrue(after > cur);
 	}
 
 	@Test
 	public void takeOut() {
 		int cur = port.refill(20);
 		int after = port.takeOut(1);
-		assertTrue(after < cur);
+		Assert.assertTrue(after < cur);
 	}
 }
