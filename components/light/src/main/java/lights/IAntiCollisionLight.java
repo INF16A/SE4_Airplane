3ca56eda9b2e06ba88072e9c48a8f11b9a60c91c
Endrit Çallaki <endritcallaki@hotmail.com>
2018-01-30 11:27:21 +0100

    AntiCollisionLight Interface

---
+++ components/light/src/main/java/lights/IAntiCollisionLight.java
@@ -0,0 +1,9 @@
+package main.java.lights;
+/**
+ * Created by Endrit Callaki on 30.01.2018.
+ */
+public interface IAntiCollisionLight {
+    public String version();
+    public boolean on();
+    public boolean off();
+}
