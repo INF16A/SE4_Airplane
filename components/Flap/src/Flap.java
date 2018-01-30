f962a5695d69e8002b549042f2239670a9823323
Manuel Libal <manuellibal@googlemail.com>
2018-01-30 12:59:16 +0100

    Füllung der Methoden Flap und Slat

--- components/Flap/src/Flap.java
+++ components/Flap/src/Flap.java
@@ -1,31 +1,63 @@
-public class Flap implements IFlap {
+public class Flap {
     private String manufacturer;
     private String type;
     private String id;
     private int degree;
 
-    @Override
-    public String version() {
-        return null;
+    public Port port;
+
+    public class Port implements IFlap {
+        @Override
+        public String version() {
+            return innerVersion();
+        }
+
+        @Override
+        public int neutral() {
+            return innerNeutral();
+        }
+
+        @Override
+        public int levelOne() {
+            return innerLevelOne();
+        }
+
+        @Override
+        public int levelTwo() {
+            return innerLevelTwo();
+        }
+
+        @Override
+        public int levelThree() {
+            return innerLevelThree();
+        }
     }
 
-    @Override
-    public int neutral() {
-        return 0;
+    private int innerLevelThree() {
+        degree = 45;
+        return degree;
     }
 
-    @Override
-    public int levelOne() {
-        return 0;
+    private int innerLevelTwo() {
+        degree = 30;
+        return degree;
     }
 
-    @Override
-    public int levelTwo() {
-        return 0;
+    private int innerLevelOne() {
+        degree = 15;
+        return degree;
     }
 
-    @Override
-    public int levelThree() {
-        return 0;
+    private int innerNeutral() {
+        degree = 0;
+        return degree;
+    }
+
+    private Flap() {
+        port = new Port();
+    }
+
+    private String innerVersion() {
+        return "Flap XYZ";
     }
 }
--- components/Slat/src/Slat.java
+++ components/Slat/src/Slat.java
@@ -1,31 +1,69 @@
-public class Slat implements ISlat {
+public class Slat {
     private String manufacturer;
     private String type;
     private String id;
     private int degree;
 
-    @Override
-    public String version() {
-        return null;
+    public Port port;
+
+    public class Port implements ISlat {
+        @Override
+        public String version() {
+            return innerVersion();
+        }
+
+        @Override
+        public int neutral() {
+            return innerNeutral();
+        }
+
+        @Override
+        public int fullDown() {
+            return innerFullDown();
+        }
+
+        @Override
+        public int down(int degree) {
+            return innerDown(degree);
+        }
+
+        @Override
+        public int up(int degree) {
+            return innerUp(degree);
+        }
     }
 
-    @Override
-    public int neutral() {
-        return 0;
+    private int innerUp(int innerDegree) {
+        degree+= innerDegree;
+        if(degree>45){
+            degree=45;
+        }
+        return degree;
     }
 
-    @Override
-    public int fullDown() {
-        return 0;
+    private int innerDown(int innerDegree) {
+        degree-= innerDegree;
+        if(degree<-45){
+            degree=45;
+        }
+        return degree;
     }
 
-    @Override
-    public int down(int degree) {
-        return 0;
+    private int innerFullDown() {
+        degree = -45;
+        return degree;
     }
 
-    @Override
-    public int up(int degree) {
-        return 0;
+    private int innerNeutral() {
+        degree = 0;
+        return degree;
+    }
+
+    private Slat() {
+        port = new Port();
+    }
+
+    private String innerVersion() {
+        return "Slat XYZ";
     }
 }
