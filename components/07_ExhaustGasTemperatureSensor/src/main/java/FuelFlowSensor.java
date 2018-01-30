bf8c56fc556384f74b43e878ca7c73c74d425dd5
armbeck16 <arm.beck.16@lehre.mosbach.dhbw.de>
2018-01-30 13:53:57 +0100

    fuel flow sensor portified

--- components/07_ExhaustGasTemperatureSensor/src/main/java/FuelFlowSensor.java
+++ components/07_ExhaustGasTemperatureSensor/src/main/java/FuelFlowSensor.java
@@ -1,4 +1,8 @@
-public class FuelFlowSensor implements  IFuelFlowSensor {
+public class FuelFlowSensor {
+    private FuelFlowSensor() {
+
+    }
+
     public String version() {
         return "super fuel flow sensor extreme edition TM";
     }
@@ -6,9 +10,24 @@ public class FuelFlowSensor implements  IFuelFlowSensor {
     public int measure() {
         return fuelFlow;
     }
-    private  String manufacturer="DHBW";
-    private  String type="zero";
-    private  String id="0";
-    private  int fuelFlow=10;
 
+    private String manufacturer = "DHBW";
+    private String type = "zero";
+    private String id = "0";
+    private int fuelFlow = 10;
+    public Port port = new Port();
+    private static FuelFlowSensor instance = new FuelFlowSensor();
+
+    public class Port implements IFuelFlowSensor {
+
+        @Override
+        public String version() {
+            return instance.version();
+        }
+
+        @Override
+        public int measure() {
+            return instance.measure();
+        }
+    }
 }
