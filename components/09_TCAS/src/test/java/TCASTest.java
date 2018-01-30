c2cdbf543ee10bf393469cc8cc35e5c325be7607
Eni <j0_s3f@web.de>
2018-01-30 18:27:28 +0100

    Unit Tests for sensor03 Components

---
+++ components/09_TCAS/src/test/java/TCASTest.java
@@ -0,0 +1,38 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TCASTest {
+
+    TCAS.Port port = TCAS.getInstance().port;
+
+    @Test
+    public void on(){
+        Assert.assertTrue(port.on());
+    }
+
+    @Test
+    public void off(){
+        Assert.assertFalse(port.off());
+    }
+
+    @Test
+    public void connect(){
+        Assert.assertEquals("drei".hashCode()%10==0,port.connect("drei"));
+    }
+
+    @Test
+    public void scan(){
+        Assert.assertTrue(port.scan("Sharknado"));
+    }
+
+    @Test
+    public void alarm(){
+        Assert.assertFalse(port.alarm());
+    }
+
+    @Test
+    public void altitude(){
+        port.setAltitude(10);
+        Assert.assertEquals(10, port.determineAltitude(""));
+    }
+}
