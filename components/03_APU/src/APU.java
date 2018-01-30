32930cf70d47db60ad511365fe1e00d370fdaed8
FlorianEder <florian.eder97@gmail.com>
2018-01-30 11:04:32 +0100

    Some changes and build.xml

--- components/03_APU/src/APU.java
+++ components/03_APU/src/APU.java
@@ -2,4 +2,53 @@
  * Created by Lenovo on 30.01.2018.
  */
 public class APU {
+    public Port port;
+
+    private String manufacturer;
+    private String type;
+    private String id;
+    private boolean isStarted;
+    private int rpm;
+
+
+    private APU(String id, String type, String manufacturer)
+    {
+        this.id = id;
+        this.type = type;
+        this.manufacturer = manufacturer;
+    }
+
+    public class Port implements IAPU
+    {
+
+        @Override
+        public String version() {
+            return "APU 1.0";
+        }
+
+        @Override
+        public boolean start() {
+            isStarted = true;
+            return isStarted;
+        }
+
+        @Override
+        public int increaseRPM(int value) {
+            rpm += value;
+            return rpm;
+        }
+
+        @Override
+        public int decreaseRPM(int value) {
+            rpm -= value;
+            return rpm;
+        }
+
+        @Override
+        public boolean shutdown() {
+            isStarted = false;
+            return isStarted;
+        }
+    }
+
 }
--- components/03_APU/src/IAPU.java
+++ components/03_APU/src/IAPU.java
@@ -2,4 +2,9 @@
  * Created by Lenovo on 30.01.2018.
  */
 public interface IAPU {
+    public String version();
+    public boolean start();
+    public int increaseRPM(int value);
+    public int decreaseRPM(int value);
+    public boolean shutdown();
 }
