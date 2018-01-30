0104c4bd28002aed2b026f16fbc96d07fdc27601
alexkrieg <alexkrieg93@googlemail.com>
2018-01-30 18:12:10 +0100

    fix

--- components/05_EngineOilTank/src/test/java/EngineOilTankTest.java
+++ components/05_EngineOilTank/src/test/java/EngineOilTankTest.java
@@ -1,5 +1,4 @@
 
-import main.java.EngineOilTank;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
---
+++ main-application/src/test/java/factory/FuelTankFactoryTest.java
@@ -0,0 +1,11 @@
+package factory;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FuelTankFactoryTest {
+	@Test
+	public void build(){
+		Assert.assertNotNull(FuelTankFactory.build());
+	}
+}
