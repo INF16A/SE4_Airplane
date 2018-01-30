6610f8c451874f27e2330dbd88ab05aa57735c1d
armbeck16 <arm.beck.16@lehre.mosbach.dhbw.de>
2018-01-30 13:53:36 +0100

    ice detector sensor portified

--- components/07_ExhaustGasTemperatureSensor/src/main/java/IceDetectorProbe.java
+++ components/07_ExhaustGasTemperatureSensor/src/main/java/IceDetectorProbe.java
@@ -1,10 +1,14 @@
-public class IceDetectorProbe implements IIceDetectorProbe {
+public class IceDetectorProbe {
+    private IceDetectorProbe() {
+
+    }
+
     public String version() {
         return "IceDetectorProbe Serious Edition";
     }
 
     public boolean activate() {
-        isActivated=true;
+        isActivated = true;
         return true;
     }
 
@@ -17,16 +21,51 @@ public class IceDetectorProbe implements IIceDetectorProbe {
     }
 
     public boolean detect(String surface, String pattern) {
-        return isIceDetected=surface.matches(pattern);
+        return isIceDetected = surface.matches(pattern);
     }
 
     public boolean deactivate() {
-        isActivated=false;
+        isActivated = false;
         return true;
     }
-    private  String manufacturer="DHBW";
-    private  String type="zero";
-    private  String id="0";
-    private  boolean isActivated=false;
-    private  boolean isIceDetected=false;
+
+    private String manufacturer = "DHBW";
+    private String type = "zero";
+    private String id = "0";
+    private boolean isActivated = false;
+    private boolean isIceDetected = false;
+    private static IceDetectorProbe instance = new IceDetectorProbe();
+    public Port port = new Port();
+
+    public class Port implements IIceDetectorProbe {
+        @Override
+        public String version() {
+            return instance.version();
+        }
+
+        @Override
+        public boolean activate() {
+            return instance.activate();
+        }
+
+        @Override
+        public boolean detect() {
+            return instance.detect();
+        }
+
+        @Override
+        public boolean detect(String surface) {
+            return instance.detect(surface);
+        }
+
+        @Override
+        public boolean detect(String surface, String pattern) {
+            return instance.detect(surface, pattern);
+        }
+
+        @Override
+        public boolean deactivate() {
+            return instance.deactivate();
+        }
+    }
 }
