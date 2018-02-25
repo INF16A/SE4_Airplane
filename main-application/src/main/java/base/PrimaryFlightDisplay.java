91ddee3d24f0a475ba39eb50aac2187eb1b76cab
felixbertsch <felixbertsch@gmx.de>
2018-02-25 22:58:45 +0100

    Merge branch 'master' of https://github.com/INF16A/SE4_Airplane into stowage_cargo
    add Factory.Build (cargoSystem /stowageNumberFive)

--- main-application/src/main/java/base/PrimaryFlightDisplay.java
+++ main-application/src/main/java/base/PrimaryFlightDisplay.java
@@ -3,7 +3,7 @@ package base;
 import java.util.ArrayList;
 
 public enum PrimaryFlightDisplay {
-    /*
+
     instance;
 
     // please add value(s)
@@ -316,5 +316,5 @@ public enum PrimaryFlightDisplay {
     public int seatManagementNumberOfPassengersFirstClassAssigned;
     public int seatManagementNumberOfPassengersBusinessClassAssigned;
     public int seatManagementNumberOfPassengersEconomyClassAssigned;
-    */
+
 }
